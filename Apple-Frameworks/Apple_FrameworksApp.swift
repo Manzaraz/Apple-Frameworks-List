736be1d9f8d3939ac240354acb1eb7b5972688fb
Christian Manzaraz <magnusmanz@gmail.com>
2024-06-23 23:48:50 -0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Christian Manzaraz on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
