6780049924e8c9009f61c0a3e633e53dd387b8e8
Christian Manzaraz <magnusmanz@gmail.com>
2024-06-24 00:22:53 -0300

    Frameworks: Grid-start

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns) {
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+            FrameworkView(name: "App Clips", imageName: "app-clip")
+    
+        }
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+struct FrameworkView: View {
+    
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
 }
