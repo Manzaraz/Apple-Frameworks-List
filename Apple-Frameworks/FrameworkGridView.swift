b9da92791fd9ede58c2107638ac5d7487517a157
Christian Manzaraz <magnusmanz@gmail.com>
2024-06-24 00:03:51 -0300

    Apple-Framework Challenge

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Christian Manzaraz on 24/06/2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
