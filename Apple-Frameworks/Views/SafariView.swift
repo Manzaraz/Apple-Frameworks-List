58bc3259af6419dc8ff1eba26bdf68c3b624507e
Christian Manzaraz <magnusmanz@gmail.com>
2024-06-28 13:06:45 -0300

    Integrating with UIKit

---
+++ Apple-Frameworks/Views/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by Christian Manzaraz on 28/06/2024.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {  }
+}
